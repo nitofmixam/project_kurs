740731a956abab871b7ffd6d145fe89ce7037f28
Maksim Fotin <maksimfotin144@gmail.com>
2024-03-08 03:23:40 +0700

    fix main and tests_funcs and utils

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-import json
-from utils import filter_invalid_values
+from utils import load_file, filter_invalid_values, displayed_operations, displayed_sorted_operations
 
-# Загрузка данных из JSON-файла
-with open('operations.json', 'r') as file:
-    data = json.load(file)
-# Фильтрация выполненных операций и сортировка по дате
-filtered_items = filter_invalid_values(data)
-executed_operations = [op for op in filtered_items if op['state'] == 'EXECUTED']
-sorted_executed_operations = sorted(executed_operations, key=lambda value: value['date'], reverse=True)
-display_operations = sorted_executed_operations[:5]
-for operation in display_operations:
-    print(f"{operation['date']} {operation['description']}")
-    masked_card = ' '.join([operation.get('from', "***")[-8:-4], operation.get('from', "*** ")[-4:]])
-    masked_account = f"*** {operation['to'][-4:]}"
-    print(f"{masked_card} -> {masked_account}")
-    operationAmount = operation['operationAmount']
-    amount = operationAmount['amount']
-    currency = operationAmount['currency']['name']
-    print(f"{amount} {currency}")
+# Загрузить файл JSON с данными о транзакциях
+data = load_file()
+
+filtered_data = filter_invalid_values(data)
+
+executed_operations = displayed_operations(filtered_data)
+
+displayed_sorted_operations(executed_operations)
--- utils.py
+++ utils.py
@@ -1,3 +1,14 @@
+import json
+import datetime
+from datetime import datetime
+
+
+def load_file():
+    with open('operations.json', 'r', encoding="utf-8") as file:
+        data = json.load(file)
+        return data
+
+
 def filter_invalid_values(items):
     filtered_items = []
     for item in items:
@@ -6,3 +17,24 @@ def filter_invalid_values(items):
         else:
             filtered_items.append(item)
     return filtered_items
+
+
+def displayed_operations(filtered_items):
+    executed_operations = [op for op in filtered_items if op['state'] == 'EXECUTED']
+    sorted_executed_operations = sorted(executed_operations, key=lambda value: value['date'], reverse=True)
+    display_operations = sorted_executed_operations[:5]
+    return display_operations
+
+
+def displayed_sorted_operations(display_operations):
+    for operation in display_operations:
+        date = datetime.strptime(operation['date'], '%Y-%m-%dT%H:%M:%S.%f')
+        date_str = date.strftime("%d.%m.%Y")
+        print(f"{date_str} {operation['description']}")
+        masked_card = ''.join([operation.get('from', "***")[-8:-4], operation.get('from', "***")[-5:]])
+        masked_account = f"*** {operation['to'][-4:]}"
+        print(f"{masked_card} -> {masked_account}")
+        operationAmount = operation['operationAmount']
+        amount = operationAmount['amount']
+        currency = operationAmount['currency']['name']
+        print(f"{amount} {currency}")
