67d55e2cc1edd31e100ca183996c3025ec2fc8ad
Maksim Fotin <maksimfotin144@gmail.com>
2024-03-09 15:34:39 +0700

    add test_load_file and test_displayed_sorted_operations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from utils import load_file, filter_invalid_values, displayed_operations, displayed_sorted_operations
 
-# Загрузить файл JSON с данными о транзакциях
 data = load_file()
 
 filtered_data = filter_invalid_values(data)
--- test/test_funcs.py
+++ test/test_funcs.py
@@ -1,4 +1,6 @@
-from utils import filter_invalid_values, displayed_operations, displayed_sorted_operations
+from utils import filter_invalid_values, displayed_operations, load_file, displayed_sorted_operations
+import pytest
+import datetime
 
 
 def test_filter_invalid_values():
@@ -16,20 +18,36 @@ def test_displayed_operations():
         {'state': 'EXECUTED', 'date': '2021-01-03'},
     ]
     result = displayed_operations(filtered_items)
-    assert len(result) <= 5
+    assert len(result) <= 2
 
 
-def test_displayed_sorted_operations():
+# Тест для функции load_file
+def test_load_file():
+    loaded_data = load_file()
+    assert isinstance(loaded_data, list)
+
+
+def test_displayed_sorted_operations(capsys):
     display_operations = [
         {
-            'date': '2022-01-01T12:00:00.000',
+            'date': '2022-09-15T08:00:00.000',
             'description': 'Payment',
             'from': '1234567890123456',
             'to': '9876543210',
             'operationAmount': {
-                'amount': 100,
+                'amount': 100.0,
                 'currency': {'name': 'USD'}
             }
         }
     ]
+
     displayed_sorted_operations(display_operations)
+
+    captured = capsys.readouterr()
+    assert "15.09.2022 Payment" in captured.out
+    assert "3456 -> *** 3210" in captured.out
+    assert "100.0 USD" in captured.out
+
+
+if __name__ == "__main__":
+    pytest.main()
